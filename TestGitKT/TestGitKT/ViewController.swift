2f6acb5da567ee6f74d6dca73aaf6ec0ee985421
RamachandraVN <vnramachandra@gmail.com>
2022-07-08 13:10:32 +0530

    Develop (#4)
    * Initial code setup
    
    * Finished Did not work module
    
    * Localization work done

---
+++ TestGitKT/TestGitKT/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TestGitKT
+//
+//  Created by Ramachandra Naragund on 08/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Beign: 'Did not work' module
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
